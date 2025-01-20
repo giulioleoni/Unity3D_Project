214a3ac68c5813cae3918ecc19a11a59e2978caa
GiulioLV98 <giulioleoni5@gmail.com>
2025-01-20 10:07:35 +0100

    SettingsMenu ended

--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -1,14 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
+using UnityEngine;
+using TMPro;
+
 
 public class SettingsMenu : MonoBehaviour
 {
     public AudioMixer master;
+    Resolution[] resolutions;
+    public TMP_Dropdown resolutionDropdown;
 
-    public void SetVolume(float volume)
+    private void Awake()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        } 
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetGameVolume(float volume)
     {
         master.SetFloat("MasterVolume", volume);
+    }
+
+    public void SetGraphicsQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void ToggleFullScreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+    }
+
+    public void SetScreenResolution(int resolutionIndex)
+    {
+        Resolution targetResolution = resolutions[resolutionIndex];
+        Screen.SetResolution(targetResolution.width, targetResolution.height, Screen.fullScreen);
     }
 }
